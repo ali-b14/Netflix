ed5bed006dc5db962ed3a48cbba67160bdf424e7
ali-b14 <alib714@gmail.com>
2024-05-06 16:47:35 -0400

    Deleted item in JSON package, Created S3Service class and Fileservice interface

---
+++ src/main/java/com/bodleian/service/S3Service.java
@@ -0,0 +1,13 @@
+package com.bodleian.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class S3Service implements FileService {
+
+    @Override
+    public String uploadFile(MultipartFile file) {
+        //upload file to AWS S3
+    }
+}
